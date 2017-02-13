3de1805a9ef38a6b68112f7f9ad4f6df7f5e2c24
shareyin <shareyin@qq.com>
2017-02-13 10:57:46 +0800

    Add files via upload

--- StateObject.cs
+++ StateObject.cs
@@ -1,31 +1,30 @@
 using System;
-using System.Drawing;
-using System.Collections;
-using System.ComponentModel;
-using System.Windows.Forms;
-using System.Data;
 using System.Net;//新加的
 using System.Net.Sockets;//新加的
 using System.Threading;
 using System.Text;
 
-namespace Sock_Conn
+
+namespace Socket_Bin
 {
 	/// <summary>
 	/// 
 	/// </summary>
 	public class StateObject
 	{
-		public Socket workSocket = null;              // Client socket.
-		public const int BufferSize = 256;            // Size of receive buffer.
-		public byte[] buffer = new byte[BufferSize];  // Receive buffer.
-		public StringBuilder sb = new StringBuilder();// Received data string.
+		
+		public Socket workSocket = null;       // Client  socket.
+		public const int BufferSize = 1024;       // Size of receive buffer.
+		public byte[] buffer = new byte[BufferSize]; // Receive buffer.
+		public StringBuilder sb = new StringBuilder();  // Received data string.
 
 		public StateObject()
 		{
 			// 
 			// TODO: 在此处添加构造函数逻辑
 			//
+  
+
 		}
 	}
 }
